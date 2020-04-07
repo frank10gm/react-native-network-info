98da577ff8650a5849a68ea48f3092f1f808177b
Frankie <frank10gm@gmail.com>
2020-04-07 19:02:38 +0200

    fix plugin

--- android/src/main/java/com/pusherman/networkinfo/RNNetworkInfo.java
+++ android/src/main/java/com/pusherman/networkinfo/RNNetworkInfo.java
@@ -328,10 +328,10 @@ public class RNNetworkInfo extends ReactContextBaseJavaModule {
 
                         if (reachable) {
                             Log.i(TAG, "{\"host\": \"" + String.valueOf(hostName) + "\", \"ip\": \"" + String.valueOf(testIp) + "\"}");
-                            promise.resolve("{\"host\": \"" + String.valueOf(hostName) + "\", \"ip\": \"" + String.valueOf(testIp) + "\"}");
                             sendEvent("host", "{\"host\": \"" + String.valueOf(hostName) + "\", \"ip\": \"" + String.valueOf(testIp) + "\"}");
                         }
                     }
+                    promise.resolve("OK");
 
                 } catch (Exception e) {
                     Log.e(TAG, e.toString());
